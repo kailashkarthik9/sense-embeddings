49bf1fe08b15ed2f1463a87f7b0ad1df77dbcf56
Kailash Karthik S <kailashkarthik9@gmail.com>
2019-10-25 22:44:18 -0400

    Introduction of word2vec interface

---
+++ embedding_hypothesis/word2vec_interface.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+from gensim.models import KeyedVectors
+
+
+class Dataset(Enum):
+    """
+    This enumeration details all the possible sources to load word2vec embeddings from.
+    Download the corresponding models and place them in the /data directory
+    """
+    GOOGLE_NEWS = 'GoogleNews-vectors-negative300.bin'
+    TWITTER = 'word2vec_twitter_tokens'
+    # WIKIPEDIA = ''
+
+
+class Word2VecInterface:
+    """
+    This class acts as the interface to the external word2vec embeddings. All the methods used to access word2vec
+    are encapsulated here. Word2vec is accessed through Gensim
+    """
+
+    def __init__(self, dataset: Dataset):
+        self.model = KeyedVectors.load_word2vec_format('data/' + dataset.value, binary=True)
+
+    def get_word_vector(self, word):
+        """
+        This method returns the embedding for a given word
+        :param word: The word for which embedding is to be retrieved
+        :return: N dimensional vector
+        """
+        return self.model[word]
